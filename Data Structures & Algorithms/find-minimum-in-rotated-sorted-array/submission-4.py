3350786be94cc6257ff2eca5b28707a0edfd0032
Pradyumna Nunna <156878599+Pradyumna369@users.noreply.github.com>
2026-04-01 18:52:20 -0400

    Add: find-minimum-in-rotated-sorted-array - submission-4

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        # rotated array has one special property. One part is always sorted and other contains rotation.
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = (l + r) // 2
+            # Part of array which is always sorted. return left most element
+            if nums[mid] <= nums[r] and nums[l] <= nums[mid]:
+                return nums[l]
+            # Part which contains rotation. Smallest in between left and right elements
+            elif nums[mid] >= nums[l]:
+                l = mid + 1
+            elif nums[mid] <= nums[r]:
+                r = mid
+        return nums[l]
