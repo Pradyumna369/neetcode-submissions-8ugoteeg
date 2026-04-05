fa24660d288a0f556cb392cc63a177f1062a5779
Pradyumna Nunna <156878599+Pradyumna369@users.noreply.github.com>
2026-04-05 17:07:12 -0400

    Add: count-subsequences - submission-2

---
+++ Data Structures & Algorithms/count-subsequences/submission-2.py
@@ -0,0 +1,35 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        """
+        s = "(c)aa(a)(t)"
+        t = "cat"
+          c a a a t
+        c 1 1 1 1 1
+        a 0 1 2 3 3
+        t 0 0 0 0 1
+
+            r a b b b i t
+          1 1 1 1 1 1 1 1
+        r 0 1 1 1 1 1 1 1
+        a 0 0 1 1 1 1 1 1
+        b 0 0 0 1 2 3 3 3
+        b 0 0 0 1 3 6 6 6
+        i 0 0 0 0 0 0 6 6
+        t 0 0 0 0 0 0 0 6
+
+        m = 5
+        n = 
+        """
+        m = len(t)
+        n = len(s)
+        matrix = [[0] * (n + 1) for _ in range(m + 1)]
+        for k in range(n + 1):
+            matrix[0][k] = 1
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if t[i - 1] == s[j - 1]:
+                    matrix[i][j] = matrix[i][j - 1] + matrix[i - 1][j - 1]
+                else:
+                    matrix[i][j] = matrix[i][j - 1]
+        return matrix[m][n]
+
