7ef7f605880d5baed0112416b6a0539567b5bb0e
Pradyumna Nunna <156878599+Pradyumna369@users.noreply.github.com>
2026-04-10 12:59:53 -0400

    Add: lru-cache - submission-2

---
+++ Data Structures & Algorithms/lru-cache/submission-2.py
@@ -0,0 +1,49 @@
+class ListNode:
+    def __init__(self, key: int, val: int):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.first = ListNode(-1, -1)
+        self.last = ListNode(-1, -1)
+        self.first.next = self.last
+        self.last.prev = self.first
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.update(self.cache[key])
+            return self.cache[key].val
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache[key].val = value
+            self.update(self.cache[key])
+        else:
+            self.cache[key] = ListNode(key, value)
+            self.update(self.cache[key])
+
+        if len(self.cache) > self.capacity:
+            lru = self.first.next
+            self.first.next = lru.next
+            lru.next.prev = self.first
+            del self.cache[lru.key]
+    
+    def update(self, node: ListNode) -> None:
+        if node.prev:
+            prev = node.prev
+            prev.next = node.next
+            node.next.prev = prev
+        self.last.prev.next = node
+        node.prev = self.last.prev
+        self.last.prev = node
+        node.next = self.last
+        
+        
