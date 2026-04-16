bd6606231d461d0b9cae14f7eecb6f1bbb4fc99e
Pradyumna Nunna <156878599+Pradyumna369@users.noreply.github.com>
2026-04-15 23:45:04 -0400

    Add: merge-k-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-2.py
@@ -0,0 +1,42 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        '''
+        lists = [[1, 1, 2, 3, 4], [1, 3, 5],[3, 6]]
+                              ^
+                                         ^ 
+                                          ^
+        0 <= lists.length <= 1000
+        0 <= lists[i].length <= 100
+        -1000 <= lists[i][j] <= 1000
+        '''
+        if lists == [] or lists == [[]]:
+            return None
+        heap = []
+        count = 0
+        for each in lists:
+            if each:
+                first = [each.val, count, each]
+                heapq.heappush(heap, first)
+                count += 1
+        dummy = ListNode()
+        dummy.next = curr = heapq.heappop(heap)[2]
+        if curr.next:
+            heapq.heappush(heap, [curr.next.val, count, curr.next])
+            count += 1
+        while heap:
+            smallest = heapq.heappop(heap)[2]
+            curr.next = smallest
+            if smallest.next and heap and smallest.next.val <= heap[0][0]:
+                curr = smallest
+                smallest = smallest.next
+            curr = curr.next
+            if smallest.next:
+                heapq.heappush(heap, [smallest.next.val, count, smallest.next])
+                count += 1
+        return dummy.next
